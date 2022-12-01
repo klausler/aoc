eee8efaaa2c5949713873ff3fc8da76e77f74972
Peter Klausler <pklausler@nvidia.com>
2022-12-01 11:34:19 -0800

    better 2022 day

--- 2022/01ab.hs
+++ 2022/01ab.hs
@@ -1,14 +1,9 @@
-import Data.List(sort)
-combine :: [[Int]] -> [[Int]]
-combine [] = []
-combine ([]:rest) = combine rest
-combine (x:[]:rest) = x : combine rest
-combine (x:rest)
-  | (r:rest') <- combine rest = (x ++ r) : rest'
-  | otherwise = [x]
+import Data.List(groupBy, sort)
+combine2 :: [[Int]] -> [[Int]]
+combine2 vals = filter (not . null) $ concat <$> groupBy (\x y -> null x == null y) vals
 main = do
   input <- readFile "in/01.txt"
-  let combined = combine $ ((read <$>) . words) <$> lines input
+  let combined = combine2 $ ((read <$>) . words) <$> lines input
       descending = reverse $ sort $ sum <$> combined
   print $ head descending -- part A
   print $ sum $ take 3 descending -- part B
