af4c6b27723efe79bd91a2550ba808456be30731
Peter Klausler <pklausler@nvidia.com>
2022-12-01 12:26:20 -0800

    final 2022 day 1

--- 2022/01ab.hs
+++ 2022/01ab.hs
@@ -1,8 +1,8 @@
 import Data.List(groupBy, sort)
-gather = filter (not.null) . (concat <$>) . groupBy (\x y -> null x == null y)
+gather = filter (not.null.head) . groupBy (\x y -> null x == null y)
 tally = sum . (read <$>) :: [String] -> Int
 main = do
-  input <- (words <$>) <$> lines <$> readFile "in/01.txt"
+  input <- lines <$> readFile "in/01.txt"
   let descending = reverse $ sort $ tally <$> gather input
   print $ head descending -- part A
   print $ sum $ take 3 descending -- part B
