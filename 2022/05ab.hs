423785a0707594eba9136f97f094e2f7f9c5094e
Peter Klausler <pklausler@nvidia.com>
2022-12-05 09:24:57 -0800

    update 2022 day 5

--- 2022/05ab.hs
+++ 2022/05ab.hs
@@ -1,14 +1,11 @@
 import Data.List(transpose)
 import qualified Data.Map as M
-parse "" = []
-parse (' ':' ':' ':' ':rest) = ' ' : parse rest
-parse (' ':'[':ch:']':rest) = ch : parse rest
 doit partFunc state (ct, from, to) = M.adjust (boxes ++) to $ M.adjust (drop ct) from state
   where boxes = partFunc $ take ct $ state M.! from
 main = do
   input <- lines <$> readFile "in/05.txt"
   let (preamble, (_:rawMoves)) = break null input
-      stacks = dropWhile (==' ') <$> transpose (parse <$> ((' ':) <$> init preamble))
+      stacks = dropWhile (==' ') <$> (((transpose $ init preamble) !!) <$> [1, 5 .. length (head preamble)])
       start = M.fromList $ zip [1..] stacks
       moves = [(read ct, read from, read to)
               | move <- rawMoves, let ["move", ct, "from", from, "to", to] = words move]
