d0948ee84fb7982270f5e3f935fc7b1be4e9a1e0
Peter Klausler <pklausler@nvidia.com>
2022-12-14 08:34:00 -0800

    update 2022 day 14

--- 2022/14ab.hs
+++ 2022/14ab.hs
@@ -5,16 +5,22 @@ expand ((x,y):(rest@((x',y'):_)))
   | otherwise = (x,y) : expand ((x+signum(x'-x),y+signum(y'-y)) : rest)
 expand last = last
 descend maxY set (x,y)
-  | y > maxY = Nothing
+  | y > maxY = (x,y)
   | S.notMember (x,y+1) set = descend maxY set (x,y+1)
   | S.notMember (x-1,y+1) set = descend maxY set (x-1,y+1)
   | S.notMember (x+1,y+1) set = descend maxY set (x+1,y+1)
-  | otherwise = Just (x,y)
-fill maxY set
-  | Just xy <- descend maxY set (500,0) = 1 + fill maxY (S.insert xy set)
-  | otherwise = 0
+  | otherwise = (x,y)
+fillA maxY set
+  | y > maxY = 0
+  | otherwise = 1 + fillA maxY (S.insert (x,y) set)
+  where (x,y) = descend maxY set (500,0)
+fillB maxY set
+  | xy == (500,0) = 1
+  | otherwise = 1 + fillB maxY (S.insert xy set)
+  where xy = descend maxY set (500,0)
 main = do
   input <- ((parse <$>) . lines) <$> readFile "in/14.txt"
   let maxY = maximum $ snd <$> (concat input)
       set = S.fromList $ input >>= expand
-  print $ fill maxY set
+  print $ fillA maxY set -- part A
+  print $ fillB maxY set -- part B
