9880df17e3b0c8d21e13b1c409a9ec6f56fd8501
Peter Klausler <pklausler@nvidia.com>
2023-01-12 13:01:34 -0800

    2019 day 5

--- 2019/02ab.hs
+++ 2019/02ab.hs
@@ -1,9 +1,7 @@
 import IntCode
 import qualified Data.Map as M
 main = do
-  input <- readFile "in/02.txt"
-  let codes = (read $ '[' : input ++ "]") :: [Int]
-      m = load codes
-      runnv n v = run (M.insert 1 n $ M.insert 2 v m) 0
+  m <- load <$> readFile "in/02.txt"
+  let runnv n v = (fst $ run (M.insert 1 n $ M.insert 2 v m) 0 [] []) M.! 0
   print $ runnv 12 2 -- part A
   print $ head [ 100 * n + v | n <- [0..99], v <- [0..99], runnv n v == 19690720 ] -- part B
--- 2019/intcode.hs
+++ 2019/intcode.hs
@@ -1,13 +1,27 @@
 module IntCode(load, run) where
 import qualified Data.Map as M
-load :: [Int] -> M.Map Int Int
-run :: M.Map Int Int -> Int -> Int
-load = M.fromList . zip [0..]
-run m at
-  | op == 99 = m M.! 0
-  | op == 1 = run (M.insert to (left + right) m) $ at + 4
-  | op == 2 = run (M.insert to (left * right) m) $ at + 4
+load :: String -> M.Map Int Int
+run :: M.Map Int Int -> Int -> [Int] -> [Int] -> (M.Map Int Int, [Int])
+load str = M.fromList $ zip [0..] $ read $ '[' : str ++ "]"
+run m at input output
+  | opc == 99 = (m, reverse output)
+  | opc == 1 = dyadic (+)
+  | opc == 2 = dyadic (*)
+  | opc == 3 = run (M.insert opd1 (head input) m) (at + 2) (tail input) output
+  | opc == 4 = run m (at + 2) input $ left : output
+  | opc == 5 = branch (/= 0)
+  | opc == 6 = branch (== 0)
+  | opc == 7 = dyadic $ truth (<)
+  | opc == 8 = dyadic $ truth (==)
   where op = m M.! at
-        left = m M.! (m M.! (at + 1))
-        right = m M.! (m M.! (at + 2))
-        to = m M.! (at + 3)
+        (modes,opc) = op `divMod` 100
+        [opd1, opd2, opd3] = [ m M.! (at + j) | j <- [1..3] ]
+        (modes', leftMode) = modes `divMod` 10
+        (modes'', rightMode) = modes' `divMod` 10
+        get 0 opd = m M.! opd
+        get 1 opd = opd
+        left = get leftMode opd1
+        right = get rightMode opd2
+        dyadic f = run (M.insert opd3 (f left right) m) (at + 4) input output
+        truth f x y = if f x y then 1 else 0
+        branch f = run m (if f left then right else at + 3) input output
